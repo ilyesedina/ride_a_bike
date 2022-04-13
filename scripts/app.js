bfd166398691839746dd71ee2c244bdd0108ddad
ilyesedina <44023617+ilyesedina@users.noreply.github.com>
2022-04-13 11:33:16 +0200

    new section fade in animation on scroll css

--- scripts/app.js
+++ scripts/app.js
@@ -54,3 +54,35 @@ setTimeout(() => hero_slide_items[0].classList.add('active'), 200);
 //     nextSlide()
 // }, 5000);
 
+// change header style when scroll
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.classList.add('shrink')
+    } else {
+        header.classList.remove('shrink')
+    }
+})
+
+// element show on scroll
+
+let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
+
+let el_to_show = document.querySelectorAll('.show-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    let distance = 200
+
+    return (rect.top <= (window.innerHeight - distance || document.documentElement.clientHeight - distance))
+}
+
+loop = () => {
+    el_to_show.forEach(el => {
+        if (isElInViewPort(el)) el.classList.add('show')
+    })
+
+    scroll(loop)
+}
+
+loop()
